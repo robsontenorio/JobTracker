cdb56dace610869ced545b4fdfb2b0df40697318
Robson Tenório <rrtenorio@gmail.com>
2022-07-09 12:28:31 -0300

    Initial Commit

---
+++ JobTracker/JobTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobTrackerApp.swift
+//  JobTracker
+//
+//  Created by Robson Tenório on 09/07/22.
+//
+
+import SwiftUI
+
+@main
+struct JobTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
